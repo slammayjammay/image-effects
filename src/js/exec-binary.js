475e60e360656708301fe77695f0e80265508870
slammayjammay <slammayjammay@gmail.com>
2017-04-05 17:52:37 -0700

    Fix ffmpeg binary for windows

--- src/js/exec-binary.js
+++ src/js/exec-binary.js
@@ -1,16 +1,26 @@
+import os from 'os';
 import { join } from 'path';
 import { exec } from 'child_process';
 const { app } = window.require('electron').remote;
 
-const COMMANDS = {
-	ffprobe: join(app.getAppPath(), './assets/ffmpeg/3.2.4/bin/ffprobe')
+const BINARIES = {
+	ffprobe: {
+		darwin: './assets/darwin/ffmpeg/3.2.4/bin/ffprobe',
+		win32: './assets/win32/ffmpeg-3.2.4-win64-static/bin/ffprobe.exe'
+	}
 };
 
 export default (command, options, callback) => {
-	if (!COMMANDS[command]) {
+	let binary = BINARIES[command];
+	if (!binary) {
 		throw new Error(`There is no binary named ${command}`);
 	}
 
-	command = COMMANDS[command];
+	let pathToBinary = binary[os.platform()];
+	if (!pathToBinary) {
+		throw new Error(`Unrecognized platform ${platform}`);
+	}
+
+	command = join(app.getAppPath(), pathToBinary);
 	exec(command + ' ' + options, callback);
 }
