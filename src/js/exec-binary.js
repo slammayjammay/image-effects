8a9ecf60034166df6d8acdfd970f31b6eaf81428
slammayjammay <slammayjammay@gmail.com>
2017-06-07 20:52:10 -0700

    Add option to return child process

--- src/js/exec-binary.js
+++ src/js/exec-binary.js
@@ -1,16 +1,19 @@
 import os from 'os';
 import { join } from 'path';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 const { app } = window.require('electron').remote;
 
 const BINARIES = {
 	ffprobe: {
 		darwin: './assets/darwin/ffmpeg/3.2.4/bin/ffprobe',
 		win32: './assets/win32/ffmpeg-3.2.4-win64-static/bin/ffprobe.exe'
+	},
+	ffmpeg: {
+		darwin: './assets/darwin/ffmpeg/3.2.4/bin/ffmpeg'
 	}
 };
 
-export default (command, options, callback) => {
+export default (command, commandOptions, options = {}) => {
 	let binary = BINARIES[command];
 	if (!binary) {
 		throw new Error(`There is no binary named ${command}`);
@@ -22,5 +25,18 @@ export default (command, options, callback) => {
 	}
 
 	command = join(app.getAppPath(), pathToBinary);
-	exec(command + ' ' + options, callback);
+
+	if (options.returnProcess) {
+		return spawn(command, commandOptions.split(' '));
+	}
+
+	return new Promise((resolve, reject) => {
+		exec(command + ' ' + commandOptions, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(data);
+			}
+		});
+	});
 }
