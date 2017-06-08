673e2af312a64237e8d78aec82f0b45f7aba560e
slammayjammay <slammayjammay@gmail.com>
2017-06-07 20:49:47 -0700

    Refactor

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { join } = require('path');
+const { format } = require('url');
 
-let win
+let win;
 
 const createWindow = () => {
 	win = new BrowserWindow({ width: 1200, height: 800 });
 
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
+	win.loadURL(format({
+		pathname: join(__dirname, 'index.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
@@ -24,4 +24,3 @@ app.on('windows-all-closed', () => process.platform !== 'darwin' && app.quit());
 
 // some mac shit
 app.on('activate', () => win === null && createWindow());
-
